ba56cc0ec7449e6f188ae9c1ef50234c6bcde317
真人 <weizhen.wang@duitang.com>
2016-08-19 18:39:49 +0800

    update

---
+++ neural_style.py
@@ -0,0 +1,65 @@
+import vgg
+
+import tensorflow as tf
+import numpy as np
+
+from sys import stderr
+
+CONTENT_WEIGHT = 5e0
+STYLE_WEIGHT = 1e2
+TV_WEIGHT = 1e2
+LEARNING_RATE = 1e1
+STYLE_SCALE = 1.0
+ITERATIONS = 1000
+VGG_PATH = 'imagenet-vgg-verydeep-19.mat'
+
+
+def main():
+
+    import sys
+    #print sys.path[0]
+    #print options.network
+    #print os.path.isfile(options.network+'/'+options.network)
+    #if not os.path.isfile(options.network):
+    #    parser.error("Network %s does not exist. (Did you forget to download it?)" % options.network)
+
+    content_image = imread('./content.jpg')
+    style_images = [imread('./style.jpg')]
+
+    width = None
+    if width is not None:
+        new_shape = (int(math.floor(float(content_image.shape[0]) /
+                content_image.shape[1] * width)), width)
+        content_image = scipy.misc.imresize(content_image, new_shape)
+    target_shape = content_image.shape
+    for i in range(len(style_images)):
+        style_scale = STYLE_SCALE
+        if options.style_scales is not None:
+            style_scale = options.style_scales[i]
+        style_images[i] = scipy.misc.imresize(style_images[i], style_scale *
+                target_shape[1] / style_images[i].shape[1])
+
+    style_blend_weights = None
+    if style_blend_weights is None:
+        # default is equal weights
+        style_blend_weights = [1.0/len(style_images) for _ in style_images]
+    else:
+        total_blend_weight = sum(style_blend_weights)
+        style_blend_weights = [weight/total_blend_weight
+                               for weight in style_blend_weights]
+
+    initial = options.initial
+    if initial is not None:
+        initial = scipy.misc.imresize(imread(initial), content_image.shape[:2])
+
+    if options.checkpoint_output and "%s" not in options.checkpoint_output:
+        parser.error("To save intermediate images, the checkpoint output "
+                     "parameter must contain `%s` (e.g. `foo%s.jpg`)")
+
+def imread(path):
+    return scipy.misc.imread(path).astype(np.float)
+
+
+def imsave(path, img):
+    img = np.clip(img, 0, 255).astype(np.uint8)
+    scipy.misc.imsave(path, img)
